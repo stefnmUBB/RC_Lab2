259ebed2e6635da09beecb744a5395675c047e15
Ștefan Neacșu-Miclea <91871146+stefnmUBB@users.noreply.github.com>
2022-10-20 13:01:56 +0300

    Update client_1b1_stefan.c

--- Lab3/1b1/client_1b1_stefan.c
+++ Lab3/1b1/client_1b1_stefan.c
@@ -1,60 +1,59 @@
-/*
-Un client trimite unui server un sir de numere. Serverul va returna clientului suma numerelor primite.
-*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv){	
+void send_string(int c, const char* str) {
+	while(*str)
+		send(c, str++, 1, 0);
+	send(c, str, 1, 0);
+}
 
-	int n;
-	struct sockaddr_in server;
+char* recv_string(int c) {
+	char* rec = (char*)malloc(256);
+	do {
+		recv(c, rec, 1, 0);
+	} while(*rec != '\0');
+	return rec;
+}
+
+int recv_int(int c) {
+	int x;
+	recv(c, &x, sizeof(x), 0);
+	return ntohs(x);
+}
+
+int main(int argc, char** argv) {	
+	
 	int c;
+	struct sockaddr_in server;
+	
 	c = socket(AF_INET, SOCK_STREAM, 0);
-	if(c < 0){
-	   printf("Eroare la crearea socket-ului client/n");
-	   return 1;
-	}   
-	
-	if(argc!=3) 
-	{
-		printf("Invalid arguments...\n");
-		return -1;		
-	}
-
-	memset(&server, 0, sizeof(server));
-	server.sin_port = htons(atoi(argv[2]));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(argv[1]);
-
-	if (connect(c, (struct sockaddr *) &server, sizeof(server)) < 0) {
-	 printf("Eroare la conectarea la server\n");
-	 return 1;
+	if(c<0) {
+		printf("Eroare la creare socket\n");
+		return 1;
 	}
 	
-	//n=10;	
-	scanf("%i",&n);
-	int m = n;	
-	n = htons(n);
-	send(c, &n, sizeof(n), 0);   
-	  
-	for(int i=0;i<m;i++)
-	{
-		int nr;
-		scanf("%i",&nr);
-		nr = htons(nr);
-		send(c, &nr, sizeof(nr), 0);		
-	}
-	  
-	printf("Suma nr este");
-	int suma;
-	recv(c, &suma, sizeof(suma), MSG_WAITALL);
-	suma = ntohs(suma);
-	printf("Suma nr este %d", suma);
+	 memset(&server, 0, sizeof(server));
+	 server.sin_port = htons(atoi(argv[2]));
+	 server.sin_family = AF_INET;
+	 server.sin_addr.s_addr = inet_addr(argv[1]);
+	
+	if(connect(c, (struct sockaddr*)&server, sizeof(server))<0) {
+		printf("Eroare la conectare server\n");
+		return 1;
+	}	
+		
+	char* str = "Ana are mere\0";
+	printf("%s\n",str);	
+	send_string(c,str);
+	
+	int result = recv_int(c);
+	
+	printf("Numarul de spatii este %i\n", result);
 
 	close(c);
-
 }
